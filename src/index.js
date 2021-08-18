0b3ffed87743063900212e5d0645edf1a651ec93
Kirill Lipnitskiy <kriskip12393@gmail.com>
2021-08-18 17:59:40 +0300

    feat: towelSort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+
+    if(!matrix || !matrix.length){
+        return [];
+    }
+
+
+    matrix.map((item, index) => {
+        if(index % 2 === 1){
+            item.sort((a, b) => b - a);
+        }
+    });
+    return matrix.join().split(',');
 }
